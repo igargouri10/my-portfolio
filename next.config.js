d8721884d3eb4d543285e62e264226daf93682b8
Ismail Gargouri <igargouri1@gmail.com>
2025-05-20 00:01:53 -0500

    Build static site with Next.js output: export

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const nextConfig = {
+    output: 'export',
+    images: {
+      unoptimized: true,
+    },
+    ...(process.env.NODE_ENV === 'production' ? {
+      basePath: '/my-portfolio',
+      assetPrefix: '/my-portfolio',
+    } : {})
+  };
+  
+  module.exports = nextConfig;
